1d4b8e0edb36368710506ae08069352efbbb6b5a
hro19 <fourbasic19@gmail.com>
2024-04-20 14:24:34 +0900

    ユーザーテーブルの削除ボタンに削除処理を追加

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import React from "react";
 import { User } from "@/types/User";
 
 const UserTable = ({ user }: {user:User}) => {
+  const deleteHandler = async () => {
+    try {
+      const response = await fetch(`https://jwt-mongo.vercel.app/api/v1/users/${user._id}`, {
+        method: 'DELETE',
+        cache: 'no-store'
+      });
+      if (response.ok) {
+        // Redirect to the same page
+        window.location.reload();
+      }
+    } catch (error) {
+      // Handle fetch error
+    }
+  };
+
   return (
     <>
       <div className="border rounded-md p-4 mb-4">
         <h2 className="text-lg font-semibold">{user._id}</h2>
         <p className="text-gray-600">【ユーザー名】{user.username}</p>
-        <button className="bg-lime-600 text-white mt-4 px-3 py-2 rounded-xl hover:opacity-70">
+        <button onClick={deleteHandler} className="bg-lime-600 text-white mt-4 px-3 py-2 rounded-xl hover:opacity-70">
           削除
         </button>
       </div>
